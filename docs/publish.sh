ca5d025b4407d75b6e856603fef729797c5f9e87
zkaifan <z@zhoukaifan.com>
2024-12-27 03:44:39 +0000

    publish

--- docs/publish.sh
+++
@@ -1,7 +0,0 @@
-rm -rf /zkaif.github.io/github/docs
-cd /zkaif.github.io
-jekyll build --destination github/docs
-cd /zkaif.github.io/github/docs
-git add .
-git commit . -m "publish"
-git push
--- docs/testRefresh.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /zkaif.github.io/_site
-cd /zkaif.github.io
-git pull
-jekyll serve --host 0.0.0.0
